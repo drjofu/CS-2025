acc5255fc8b68aa28e5041394217ec50b2896e99
drjofu <drjofu@web.de>
2025-04-29 10:31:53 +0200

    Tag 2

--- InterfacesUndDelegates/InterfacesUndDelegates/Program.cs
+++ InterfacesUndDelegates/InterfacesUndDelegates/Program.cs
@@ -51,6 +51,7 @@ List<Konto> konten = new List<Konto>()
 Erweiterungsmethoden.Ausgeben(konten);
 
 konten.Ausgeben("via Extension Method");
+
 new int[] { 1, 2, 3 }.Ausgeben();
 "Hallo".Ausgeben("String");
 
@@ -59,4 +60,59 @@ new int[] { 1, 2, 3 }.Ausgeben();
 konten.Sort();
 konten.Ausgeben("Sortiert");
 
+Comparison<Konto> vergleichsmethode = new Comparison<Konto>(VergleicheNachSaldo);
+konten.Sort(vergleichsmethode);
+konten.Ausgeben("Sortiert nach Saldo");
+
+konten.Sort(VergleicheNachSaldo);
+konten.Ausgeben("Sortiert nach Saldo");
+
+// Anonyme Methode C# 2.0
+int richtung = -1;
+konten.Sort(delegate (Konto k1, Konto k2)
+{
+  return richtung * k1.Inhaber!.CompareTo(k2.Inhaber);  // richtung als Closure
+});
+
+konten.Ausgeben("Sortiert nach Inhaber");
+
+// Lambda-Ausdruck C# 3.0
+konten.Sort((k1, k2) =>
+{
+  return richtung * k1.Inhaber!.CompareTo(k2.Inhaber);
+});
+
+konten.Ausgeben("Sortiert nach Inhaber");
+
+konten.Sort((k1, k2) => richtung * k1.Inhaber!.CompareTo(k2.Inhaber));
+konten.Ausgeben("Sortiert nach Inhaber");
+
+Konto? kontoDagobert = konten.Find(k => k.Inhaber == "Dagobert");
+Console.WriteLine($"Gefunden: {kontoDagobert}");
+
+konten.FindAll(k => k.Inhaber!.StartsWith("T")).Ausgeben("Alle mit T");
+
+Console.WriteLine();
+
+var ergebnis = konten
+      .Where(k => k.Inhaber!.StartsWith("T"))
+      .OrderByDescending(k => k.Kontonummer)
+      .Select(k => new {k.Inhaber, k.Kontonummer});
+
+
+      ergebnis.Ausgeben("Alle mit T");
+
+//IEnumerable<Konto> kontoEnumerable = konten;
+//IEnumerator<Konto> kontoEnumerator = kontoEnumerable.GetEnumerator();
+//while (kontoEnumerator.MoveNext())
+//{
+//  Konto konto = kontoEnumerator.Current;
+//  Console.WriteLine(konto.Inhaber);
+//}
+
 Console.ReadLine();
+
+int VergleicheNachSaldo(Konto k1, Konto k2)
+{
+  return k1.Saldo.CompareTo(k2.Saldo);
+}

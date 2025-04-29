faef79ca582e6d445ee49f5a4ef9d9e363e11127
drjofu <drjofu@web.de>
2025-04-29 16:52:13 +0200

    Tag 2

---
+++ VererbungBeispiel/VererbungBeispiel/Program.cs
@@ -0,0 +1,66 @@
+﻿
+B b = new B();
+A a = b;
+
+a.TuWas();
+b.TuWas();
+
+IDingsbums d = b;
+d.TuWasAnderes();
+
+
+string text = "Hallo Welt";
+Console.WriteLine(text.ToUpper());
+
+Console.WriteLine("😀😀😊😫😁 \u1000");
+
+text = @"eins
+         zwei
+         drei";
+
+Console.WriteLine(text);
+Console.ReadLine();
+
+class A
+{
+  public A(string text)
+  {
+    Console.WriteLine("CTOR A");
+    //TuWas();
+  }
+  public virtual void TuWas()
+  {
+    Console.WriteLine("TuWas A");
+  }
+}
+
+sealed class B : A, IDingsbums
+{
+  public B() : base("Hallo Welt")
+  {
+    Console.WriteLine("CTOR B");
+  }
+
+  public override void TuWas()
+  {
+    Console.WriteLine("TuWas B");
+    //base.TuWas();
+  }
+
+  public void TuWasAnderes()
+  {
+    Console.WriteLine("TuWasAnderes B");
+  }
+}
+
+interface IDingsbums
+{
+  void TuWasAnderes() { Console.WriteLine("Hallo"); }
+}
+
+// MustInherit NotInheritable
+//public abstract sealed class C
+public static class C
+{
+
+}

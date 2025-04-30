6b84a5f63953090caa65be37d973a012589a9850
drjofu <drjofu@web.de>
2025-04-30 14:03:02 +0200

    Tag 3

--- AsyncBeispiele/AsyncBeispiele/Window1.xaml.cs
+++ AsyncBeispiele/AsyncBeispiele/Window1.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace AsyncBeispiele
 {
@@ -25,20 +12,66 @@ namespace AsyncBeispiele
       InitializeComponent();
     }
 
-    private void BTN_START_Click(object sender, RoutedEventArgs e)
-    {
-      BTN_START.IsEnabled = false;
-      Task
-        .Run(Inkrementieren)
-        .ContinueWith(t => BTN_START.IsEnabled = true, TaskScheduler.FromCurrentSynchronizationContext());
-      //BTN_START.IsEnabled = true;
+    private CancellationTokenSource cts;
+    private CancellationToken cancellationToken;
 
+    private async void BTN_START_Click(object sender, RoutedEventArgs e)
+    {
+      cts = new CancellationTokenSource();
+      try
+      {
+        cancellationToken = cts.Token;
+
+        BTN_START.IsEnabled = false;
+        BTN_STOPP.IsEnabled = true;
+
+        //Task
+        //  .Run(Inkrementieren)
+        //  .ContinueWith(t => BTN_START.IsEnabled = true, TaskScheduler.FromCurrentSynchronizationContext());
+
+        Task task= Task.Run(Inkrementieren, cancellationToken);
+        try
+        {
+          await task.ConfigureAwait(true);
+
+          int x = await Berechnen(cancellationToken);
+          LBL.Content = x;
+          await Task.Delay(2000, cancellationToken);
+        }
+        catch (Exception ex)
+        {
+
+        }
+
+        LBL.Content=task.Status;
+
+        BTN_START.IsEnabled = true;
+        BTN_STOPP.IsEnabled = false;
+      }
+      finally
+      {
+        cts.Dispose();
+      }
+    }
+
+    async Task<int> Berechnen(CancellationToken cancellationToken)
+    {
+      await Task.Delay(1000, cancellationToken);
+      return 42;
     }
 
     private void Inkrementieren()
     {
+      //throw new ApplicationException("hoppla...");
+
       for (int i = 0; i < 5; i++)
       {
+        if (cancellationToken.IsCancellationRequested)
+        {
+          // aufräumen...
+          cancellationToken.ThrowIfCancellationRequested();
+        }
+
         Thread.Sleep(1000);
         // LBL.Content = i;
         //Dispatcher.BeginInvoke(new Action<int>(x => LBL.Content = x), i);
@@ -48,6 +81,11 @@ namespace AsyncBeispiele
         Dispatcher.BeginInvoke(() => LBL.Content = index);
         //Dispatcher.BeginInvoke(() => Debug.WriteLine(index));
       }
+    }
+
+    private void BTN_STOPP_Click(object sender, RoutedEventArgs e)
+    {
+      cts?.Cancel();
     }
   }
 }
